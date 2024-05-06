023c1f9e13594e23b5aec7830e44003401c49472
Dayana <dayana.nashkova@yahoo.com>
2024-05-06 17:36:30 +0000

    feat: get user input values

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const form = document.getElementById('input-form');
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //   get the user input
+  const text = document.getElementById('input-text').value;
+  // get the checked radio btn (language)
+  let languageBtn = document.querySelector('input:checked');
+  if (languageBtn) {
+    languageBtn = languageBtn.value;
+  }
+
+  console.log(text);
+  console.log(languageBtn);
+});
