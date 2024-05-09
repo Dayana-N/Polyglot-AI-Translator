254d7d2e0b5ccfbd70545731f35450210bea71af
Dayana <dayana.nashkova@yahoo.com>
2024-05-09 23:14:31 +0000

    feat: make a call to openai api

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+import OpenAI from 'openai';
+// import { openai } from './node_modules';
+
 const form = document.getElementById('input-form');
+const inputContainer = document.getElementById('content');
+const translationContainer = document.getElementById('translation');
+const openai = new OpenAI({
+  apiKey: 'sk-key',
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -9,7 +17,24 @@ form.addEventListener('submit', (e) => {
   if (languageBtn) {
     languageBtn = languageBtn.value;
   }
+  // Hide original screen and display translation screen
+  inputContainer.classList.add('d-none');
+  translationContainer.classList.remove('d-none');
+
+  // Make the call to OpenAi API
+  getTranslation();
 
   console.log(text);
   console.log(languageBtn);
 });
+
+async function getTranslation() {
+  const completion = await openai.chat.completitions({
+    messages: [
+      { role: 'system', content: 'You are experienced translator' },
+      { role: 'user', content: `Translate ${text} to ${languageBtn}` },
+    ],
+    model: 'gpt-3.5-turbo',
+  });
+  console.log(completion.choices[0].message.content);
+}
